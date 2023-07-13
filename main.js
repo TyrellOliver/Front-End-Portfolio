618ccb7f8db0b6a3ee5b7808210b8199d4d1704f
Tyrell <tyrellkamara-oliver@pursuit.org>
2023-07-13 03:33:19 -0400

    Was able to get some fire type pokemon when clicked. Still working on the bugs

--- main.js
+++ main.js
@@ -1,27 +1,72 @@
 // console.log( fetch('https://pokeapi.co/api/v2/'))
 
+// This fetches all of the fire type data from the pokemon api
 const fireTypePromise = fetch("https://pokeapi.co/api/v2/type/10/");
 fireTypePromise
   .then((fireTypeResponse) => {
-    return fireTypeResponse.json();
+    return fireTypeResponse.json(); // Turns that data into json
   })
   .then((data) => {
+    // usable data that we can now use dot notation on
     console.log(data);
     data.pokemon.forEach((pokemonType) => {
-      // console.log(pokemonType);
-      const pokemonPromise = fetch(pokemonType.pokemon.url);
+      //iterates over each fire type pokemon object in the array.
+      //   console.log(pokemonType);
+      const pokemonPromise = fetch(pokemonType.pokemon.url); // This fetches all of the pokemon that has a fire type
       pokemonPromise
         .then((pokemonResponse) => {
-          return pokemonResponse.json();
+          return pokemonResponse.json(); // Turns that data into json
         })
-        .then((data) => {
-          console.log(data);
-          const pokeData = data.name;
-          const mainDiv = document.querySelector(".main-div");
-          const pokeTypes = document.querySelectorAll(".poke");
-          pokeTypes.addEventListener("click", () => {
-            mainDiv.appendChild(pokeData)
-          });
+        .then((pokedata) => {
+          console.log("Data for each fire type pokemon: ", pokedata); // logs the data for each fire type pokemon
+          const name = pokedata.name;
+          const speciesUrl = pokedata.species.url;
+          const speciesPromise = fetch(speciesUrl); // Fetching the species data to get the generation and description of each pokemon
+          speciesPromise
+            .then((speciesResponse) => {
+              return speciesResponse.json();
+            })
+            .then((speciesData) => { // The data we can use
+              const generation = speciesData.generation.name;  // Use dot notation to get the generation of the pokemon
+              //   console.log(generation);
+              const pokeImg = pokedata.sprites.front_default; // Use dot notation to get the picture of the pokemon
+
+              const fireType = document.querySelector(".poke.two"); // Selecting the element that is being interacted with
+              fireType.addEventListener("click", () => {  // Adding a click event to it
+                const mainDiv = document.querySelector(".main-div"); // Selecting the div that will house the pokemon's information
+                const existingContent = mainDiv.querySelector(".poke-details");
+                 //// This is saying that if the pokemon's detail's that we're gonna create exists/ is true then remove it, because if you don't then all of the pokemon will show up on the page when clicked
+                if (existingContent) {                                          
+                  existingContent.remove();
+                }
+                const pokeDetails = document.createElement("div"); // Creates a new div to have the information inside of
+                pokeDetails.classList.add("poke-details"); // adds a class to that div
+
+                const pokeImgContainer = document.createElement("div"); // Creates a div to have  the pokemon img
+                pokeImgContainer.classList.add("pokemon-img");
+                pokeDetails.appendChild(pokeImgContainer); // Appends or puts it in the poke-details div
+
+                const nameElement = document.createElement("h2"); // Creating a h2 tag that's gonna have the pokemon's name
+                nameElement.textContent = name;
+                pokeDetails.appendChild(nameElement); // Appends or puts it in the poke-details div
+
+                const generationElement = document.createElement("p"); // Creates a p tag that has the pokemon's generation
+                generationElement.textContent = "Generation: " + generation;
+                pokeDetails.appendChild(generationElement);
+
+                const spriteImage = document.createElement("img");
+                spriteImage.src = pokeImg;
+                spriteImage.alt = name;
+                pokeImgContainer.appendChild(spriteImage);
+
+                mainDiv.appendChild(pokeDetails); // Appends all of the information in the pokeDetails to the mainDiv
+              });
+            });
+
+          //   const fireType = document.querySelectorAll(".poke.two");
+          //   fireType.addEventListener("click", () => {
+          //     mainDiv.appendChild(pokeData)
+          //   });
         });
     });
   })
